349ad2b6b7926d69631a7d0088bf722a5c5918ad
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-03-11 19:42:46 +0100

    feat: eliminata la navbar

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/Nav/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +12,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <main>{children}</main>
-        <Nav/>
       </body>
     </html>
   );
