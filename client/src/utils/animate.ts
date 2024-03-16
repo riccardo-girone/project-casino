bbaaa1b8f654fd2fcb82b71df60a793cf680d076
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-03-16 23:57:12 +0100

    feat: aggiunta animatePageOut

--- client/src/utils/animate.ts
+++ client/src/utils/animate.ts
@@ -19,6 +19,23 @@ export const animatePageIn = () => {
     }
 }
 
-export const animatePageOut = (href: String, router: AppRouterInstance) => {
+export const animatePageOut = (href: string, router: AppRouterInstance) => {
+    const bannerOne = document.getElementById("banner-1");
+    const bannerTwo = document.getElementById("banner-2");
+    const bannerThree = document.getElementById("banner-3");
+    const bannerFour = document.getElementById("banner-4");
 
+    if (bannerOne && bannerTwo && bannerThree && bannerFour) {
+        const tl = gsap.timeline()
+
+        tl.set([bannerOne, bannerTwo, bannerThree, bannerFour],{
+            yPercent: -100,
+        }).to([bannerOne, bannerTwo, bannerThree, bannerFour], {
+            yPercent: 0,
+            stagger: 0.2,
+            onComplete: () => {
+                router.push(href)
+            }
+        })
+    }
 }
