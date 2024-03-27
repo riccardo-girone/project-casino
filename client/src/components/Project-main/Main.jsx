2208f9db9027eef9ecef73df122b3ecb911c6df9
riccardo-girone <riccardoo.gironee@gmail.com>
2024-03-27 11:40:00 +0100

    feat: finito leftContent

--- client/src/components/Project-main/Main.jsx
+++ client/src/components/Project-main/Main.jsx
@@ -1,11 +1,21 @@
 import styles from './Main.module.scss';
 
 export default function Main() {
-    return(
+    return (
         <div className={styles.containerWrapper}>
             <div className={styles.leftContent}>
                 <div className={styles.class}>class</div>
                 <div className={styles.class}>main</div>
+
+                <div className={styles.textContainer}>
+                    <div className={styles.text}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar dolor aliquet at. Vestibulum diam odio, condimentum in orci sed, aliquam bibendum nulla.
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.rightContainer}>
+                
             </div>
         </div>
     );
