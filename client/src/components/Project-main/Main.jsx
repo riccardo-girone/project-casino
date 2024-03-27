69e6fcb0f3b9107572b94ee0699bebc0189269ad
riccardo-girone <riccardoo.gironee@gmail.com>
2024-03-27 11:05:35 +0100

    feat: aggiutna scritta pagina project main

--- client/src/components/Project-main/Main.jsx
+++ client/src/components/Project-main/Main.jsx
@@ -3,8 +3,10 @@ import styles from './Main.module.scss';
 export default function Main() {
     return(
         <div className={styles.containerWrapper}>
-            <div className={styles.class}>class</div>
-            <div className={styles.class}>main</div>
+            <div className={styles.leftContent}>
+                <div className={styles.class}>class</div>
+                <div className={styles.class}>main</div>
+            </div>
         </div>
     );
 }
