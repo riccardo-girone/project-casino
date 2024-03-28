ca3585463277de5b12ef5c489b16d05014ca3c41
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-03-28 19:54:24 +0100

    fix: versione finale del footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,10 +1,23 @@
 import styles from './Footer.module.scss';
 
+
 export default function Footer() {
     return(
         <div className={styles.container}>
             <div className={styles.navContent}>
-                <h2>project made by Telloli & Girone | website made by Girone</h2>
+                <a 
+                    target="_blank"
+                    href="https://github.com/Fabio-Telloli/Cavallos-Centrer-cassino">
+                    <h2 className={styles.text}>project made by Telloli & Girone</h2>
+                </a>
+
+                <h2 className={styles.text}>|</h2>
+                    
+                <a 
+                    target="_blank"
+                    href="https://github.com/riccardo-girone/project-casino">
+                    <h2 className={styles.text}>website made by Girone</h2>
+                </a>
             </div>
         </div>
     );
