69456134a41884ccf10b5d00b31e92bd6de8ba90
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-03-30 14:45:31 +0100

    feat: finito scheletro pagina prjectmain

--- client/src/components/Project-main/codeExplenetion/CodeEx.jsx
+++ client/src/components/Project-main/codeExplenetion/CodeEx.jsx
@@ -4,17 +4,28 @@ export default function CodeEx() {
     return(
         <div className={styles.container}>
             <div className={styles.title}>
-                <div className={styles.titleText}>01 main minimale</div>
+                <div>01. main minimale</div>
             </div>
 
-            <div className={styles.content1}>
-                <div className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar dolor aliquet at. Vestibulum diam odio, condimentum in orci sed, aliquam bibendum nulla.</div>
-            </div>
+            <div className={styles.contentContainer}>
+                <div className={styles.contentText}>
+                    <div>
+                      Vestibulum diam odio, condimentum in orci sed, aliquam bibendum amet, consectetur adipiscing elit. Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar dolor aliquet at. Vestibulum diam odio, condimentum in orci sed, aliquam bibendum nulla. adipiscing elit. Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar
+                    </div>
+                </div>
 
-            <div className={styles.codeContainer}></div>
+                <div className={styles.codeWrapper}>
+                    <div className={styles.codeContainer}>
 
-            <div className={styles.content2}>
-                <div className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar dolor aliquet at. Vestibulum diam odio, condimentum in orci sed, aliquam bibendum nulla.</div>
+                    </div>
+                </div>
+            
+
+                <div className={styles.contentText}>
+                    <div>
+                        Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar dolor aliquet at. Vestibulum diam odio, condimentum in orci sed, aliquam bibendum nulla. adipiscing elit. Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar dolor aliquet at. Vestibulum diam odio, condimentum in orci sed, aliquam bibendum
+                    </div>
+                </div>
             </div>
         </div>
     );
