416aeb831fc8a26029ea8eed5620097747eb9825
riccardo-girone <riccardoo.gironee@gmail.com>
2024-04-04 19:04:52 +0200

    feat: creata bozza pagina roulette

---
+++ client/src/components/Project-roulette/codeExplenetion/CodeEx.jsx
@@ -0,0 +1,32 @@
+import styles from './CodeEx.module.scss';
+
+export default function CodeEx() {
+    return(
+        <div className={styles.container}>
+            <div className={styles.title}>
+                <div>01. pippobaiudo</div>
+            </div>
+
+            <div className={styles.contentContainer}>
+                <div className={styles.contentText}>
+                    <div>
+                      Vestibulum diam odio, condimentum in orci sed, aliquam bibendum amet, consectetur adipiscing elit. Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar dolor aliquet at. Vestibulum diam odio, condimentum in orci sed, aliquam bibendum nulla. adipiscing elit. Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar
+                    </div>
+                </div>
+
+                <div className={styles.codeWrapper}>
+                    <div className={styles.codeContainer}>
+
+                    </div>
+                </div>
+            
+
+                <div className={styles.contentText}>
+                    <div>
+                        Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar dolor aliquet at. Vestibulum diam odio, condimentum in orci sed, aliquam bibendum nulla. adipiscing elit. Pellentesque fringilla tellus at nibh sagittis mattis. Fusce pulvinar nisl dictum lacus posuere porta. Mauris commodo velit magna, sed pulvinar dolor aliquet at. Vestibulum diam odio, condimentum in orci sed, aliquam bibendum
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
